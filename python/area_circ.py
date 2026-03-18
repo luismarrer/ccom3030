e75a82ac6ae1282a8b5131c397cb72236c190f0d
Luis Marrero <luismarrer.dev@gmail.com>
2026-03-18 12:35:05 -0400

    Add circle area task

---
+++ python/area_circ.py
@@ -0,0 +1,15 @@
+# Luis Marrero
+# 801-18-7010
+ 
+# Const
+PI = 3.14
+
+# Program
+
+
+radio = float(input("Entre el radio del circulo: "))
+
+
+area_circ = PI * radio ** 2
+
+print("El area del circulo es: ", area_circ)
