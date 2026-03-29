32bb7812c36ff8ccf7f066b403bc3452c2fc951b
Luis Marrero <luismarrer.dev@gmail.com>
2026-03-28 20:05:53 -0400

    feat: improve area circle calculation formatting and output

--- python/area_circ.py
+++ python/area_circ.py
@@ -1,15 +1,21 @@
 # Luis Marrero
 # 801-18-7010
+# CCOM3030-001
  
 # Const
 PI = 3.14
 
 # Program
 
+intro = "Programa para calcular el área de un círculo"
 
-radio = float(input("Entre el radio del circulo: "))
+print()
+print(intro)
+print("-" * len(intro), end="\n\n")
 
-
+radio = float(input("Entre el radio del círculo: "))
 area_circ = PI * radio ** 2
 
-print("El area del circulo es: ", area_circ)
+print()
+print("El área de un círculo con", radio, "de radio es:", area_circ, end="\n\n")
+print("Fin del programa")
