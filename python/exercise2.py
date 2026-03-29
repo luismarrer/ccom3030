b858010639701df5803c8219b50c31768dd65f13
Luis Marrero <luismarrer.dev@gmail.com>
2026-03-28 20:35:31 -0400

    feat: add exercise2.py to calculate dividend equation components

---
+++ python/exercise2.py
@@ -0,0 +1,24 @@
+# Luis Marrero
+# 801-18-7010
+# CCOM3030-001
+
+# Calcular ecuación dividendo = divisor * cociente + residuo
+
+intro = "Programa para calcular la ecuación: `dividendo = divisor * cociente + residuo`"
+
+print()
+print(intro)
+print("-" * len(intro), end="\n\n")
+
+# Pedirle al usuario un dividendo y un divisor
+# Asumir que el divisor es mayor que cero
+
+dividendo = int(input("Escriba un dividendo: "))
+divisor = int(input("Escriba un divisor: "))
+
+# imprimir `dividendo = divisor * cociente + residuo`
+
+print()
+print(f"{dividendo} = {divisor} * {dividendo//divisor} + {dividendo%divisor}")
+print()
+print("Fin del programa")
